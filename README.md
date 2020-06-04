bca52f2d66bdd7a321c1972bcd97cfc913666aa8
straker741 <straker7457@azet.sk>
2020-06-04 23:03:05 +0200

    Clean up gpioHandler + README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # termalizator
 DS18B20 temperature sensor project
 
+# Raspberry pinout
+Follow highlighted by red color center numbers!
+<img src="./raspberry_pinout.png" alt="Raspberry pinout">
+
+# Scheme of heating driver
+<img src="./termalizator.svg" alt="Termalizator">
+
 
--- gpioHandler.py
+++ gpioHandler.py
@@ -8,79 +8,87 @@ from datetime import datetime
 BASE_DIR = "/sys/bus/w1/devices/"
 
 
-def getConfig(path):
+def readFile(path):
     try:
         with open(path, 'r') as f:    
             content = f.read()
+        return content
     except:
-        print("Error: Could not get config!")
-        return ""
-    return content
+        print("Error: Could not read from file!")
+        exit()
+
+def getConfig(path):
+    try:
+        content = readFile(path)
+        return json.loads(content)
+    except:
+        print("Error: Could not load config.json")
+        exit()
 
 def setConfig(path, data):
     try:
         with open(path, 'w') as f:
             f.write(str(data))
+        return True
     except:
         print("Error: Could not set config!")
-        return False
-    return True
+        exit()
 
 def getWorkingDirectory():
     p = subprocess.Popen("pwd", stdout=subprocess.PIPE, universal_newlines=True, shell=True)
     data = p.communicate()[0].strip()
     return data
 
-def getTargetTemp():
+def getTargetTemp(wd):
     config = getConfig(wd + "/config.json")
-    config = json.loads(config)
     return config["targetTemp"]
 
+
 class ThermoReader:
-    def __init__(self, wd):
+    def __init__(self, outPin):
         try:
-            print("working Directory:", wd)
-            config = getConfig(wd + "/config.json")
-            print("config:", config)
-            config = json.loads(config)
-            self.inputPin = config["inputPin"]
-            self.outputPin = config["outputPin"]
-            self.targetTemp = config["targetTemp"]
-            
-            self.setup()
+            self.outputPin = outPin
+            self.__setup()
+            self.getIDs()
         except:
-            print("Cannot read config.json!")
-            print("Exitting!")
+            print("Error: Could not initialize TermoReader!")
             exit()
 
-    def setup(self):
-        print("SETUP")
-        GPIO.setmode(GPIO.BOARD)
-        print("Setting mode")
-        GPIO.setup(self.outputPin, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(self.inputPin, GPIO.IN)
-        print("Setting GPIOs")
-        print("Getting IDs:", end=" ")
-        self.getIDs()
+    def __setup(self):
+        try:
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.outputPin, GPIO.OUT, initial=GPIO.LOW)
+        except:
+            print("Error: Could not setup GPIO!")
+            exit()
 
     def getIDs(self):
         p = subprocess.Popen("ls " + BASE_DIR, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
         data = p.communicate()[0].split()
         self.IDs = [ x for x in data if x[0:2] == "28" ]
-        print(self.IDs)
         return self.IDs
 
-    def read_temp_raw(self, path):
+    def prettyPrint(self):
+        try:
+            i = 0
+            for row in self.data:
+                print(str(i) + ": id:", row[0], "temp:", row[1])
+                i += 1
+        except:
+            print("Error: Unable to pretty print data!")
+            exit()
+
+    def __read_temp_raw(self, path):
         f = open(path, 'r')
         lines = f.readlines()
         f.close()
         return lines
     
-    def read_temp(self, path):
-        lines = self.read_temp_raw(path)
+    def __read_temp(self, path):
+        lines = self.__read_temp_raw(path)
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = self.read_temp_raw(path)
+            lines = self.__read_temp_raw(path)
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos+2:]
@@ -91,52 +99,64 @@ class ThermoReader:
         data = []
         for ID in self.IDs:
             path = BASE_DIR + ID + '/w1_slave'
-            temp = self.read_temp(path)
+            temp = self.__read_temp(path)
             data.append([ID, temp])
         self.data = data
         return data
 
     def getAVG(self):
-        values = [ x[1] for x in self.data ]
-        avg = sum(values) / len(values)
-        return avg
+        try:
+            values = [ x[1] for x in self.data ]
+            avg = sum(values) / len(values)
+            return avg
+        except:
+            print("Error: Unable to calculate average value of temperature!")
+            print("Presumably because 'values' is empty!")
+            exit()
 
     def setOutON(self):
         GPIO.output(self.outputPin, GPIO.HIGH)
-        print("Setting outputPin HIGH", end=" ")
-        print(GPIO.gpio_function(self.outputPin))
+        print("Setting outputPin to HIGH")
 
     def setOutOFF(self):
         GPIO.output(self.outputPin, GPIO.LOW)
-        print("Setting outputPin LOW:", end=" ")
-        print(GPIO.gpio_function(self.outputPin))
+        print("Setting outputPin to LOW!")
 
 if __name__ == "__main__":
     try:
         wd = getWorkingDirectory()
-        TR = ThermoReader(wd)
-        TR.setup()
+        config = getConfig(wd + "/config.json")
+        TR = ThermoReader(config["outputPin"])
 
+        isLow = True
         while True:
             data = TR.getTempsWithIDs()
             temp = TR.getAVG()
-            targetTemp = getTargetTemp()
-            if temp < targetTemp:
+            targetTemp = getTargetTemp(wd)
+            if (temp < targetTemp and isLow):
                 TR.setOutON()
-            else:
+                isLow = False
+            elif (temp > targetTemp and not isLow):
                 TR.setOutOFF()
+                isLow = True
+            currTime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
-            
-            print("json dumps:", json.dumps(data))
+            # Logging
+            print("Current time:", currTime)
+            TR.prettyPrint()
+            print("Average temp:", temp)
+            print()
+
+            # Preparing data
             data = {
                 "data": data,
-                "datetime": datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+                "datetime": currTime
             }
-            print("data:", data)
             setConfig(wd + "/data.json", json.dumps(data))
 
             sleep(1)
     except KeyboardInterrupt:
         print("End of temperature readings!")
-        GPIO.cleanup()
-
+    except:
+        print("Generic Error")
+GPIO.cleanup()
